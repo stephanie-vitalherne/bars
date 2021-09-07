f10a24d6c5feeb269d58bb33d7b84bb10ddda46f
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-09-07 01:51:06 -0400

    navbar done

---
+++ src/app/components/navbar/menuStyles.ts
@@ -0,0 +1,46 @@
+export default {
+  bmBurgerButton: {
+    position: "absolute",
+    width: "20px",
+    height: "20px",
+    right: "20px",
+    top: "20px",
+  },
+  bmBurgerBars: {
+    background: "#373A47",
+  },
+  bmBurgerBarsHover: {
+    background: "#A90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#BDC3C7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    width: "60%",
+    height: "100%",
+    top: "0px",
+  },
+  bmMenu: {
+    background: "#373A47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373A47",
+  },
+  bmItemList: {
+    color: "#B8B7AD",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0,0,0,0.3)",
+  },
+};
