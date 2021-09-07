5959e5323f4c0c2ee4e1e3b47ed452138d9b7a11
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-09-07 04:48:52 -0400

    calendar card done. calendar package will be added later

---
+++ src/app/components/marginer/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface MarginerProps {
+  margin: number | string;
+  direction?: "horizontal" | "vertical";
+}
+
+const HorizontalMargin = styled.span<MarginerProps>`
+  display: flex;
+  min-width: ${({ margin }) =>
+    typeof margin === "string" ? margin : `${margin}px`};
+`;
+
+const VerticalMargin = styled.span<MarginerProps>`
+  display: flex;
+  min-width: ${({ margin }) =>
+    typeof margin === "string" ? margin : `${margin}px`};
+`;
+
+function Marginer(props: MarginerProps) {
+  const { direction } = props;
+
+  if (direction === "horizontal") return <HorizontalMargin {...props} />;
+  else {
+    return <VerticalMargin {...props} />;
+  }
+}
+
+Marginer.defaultProps = {
+  direction: "horizontal",
+};
+
+export { Marginer };
